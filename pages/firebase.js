f82141ed76d1d8c4e74b518a41c8395508e4d15d
majeric.matic@scptuj.si <majeric.matic@scptuj.si>
2023-05-05 10:04:44 +0200

    firease fix

--- pages/firebase.js
+++ pages/firebase.js
@@ -2,10 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-import 'firebase/auth';
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
